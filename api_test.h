c4ecd5182f130ab167dd635a6b07fdf392a5ce18
user <KurbatovVV82@mail.ru>
2023-10-04 13:28:05 +0300

    first commit

---
+++ api_test.h
@@ -0,0 +1,6 @@
+#ifndef API_TEST_H
+#define API_TEST_H
+
+int cpp_app(int argc, const char *argv[]);
+
+#endif // API_TEST_H
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "api_test.h"
+#include "c_api_test.h"
+
+int main(int argc, const char *argv[])
+{
+    //return cpp_app(argc, argv);
+    return c_app(argc, argv);
+}
+
